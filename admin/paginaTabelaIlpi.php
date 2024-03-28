733989edd30ca361880930007551ff4bb27c01d6
Julie Cristino Bremm <julie.cb2004@aluno.ifsc.edu.br>
2024-03-28 15:54:45 -0300

    - Adição de filtros a página tabela de ILPIs, sendo eles: Busca por nome ou cnpj, município e convênio - Estilização dos filtros - Ajuste hover do link/ nome e cnpj da tabela

--- admin/paginaTabelaIlpi.php
+++ admin/paginaTabelaIlpi.php
@@ -14,11 +14,62 @@
   </nav>
   </header> 
 
+  <section class="filtros">
+
+    <div title="Buscar ILPI por nome ou CNPJ">
+      <label for="filtro"><i class="fas fa-search"></i> </label>
+      <input type="text" id="filtro" placeholder="Buscar por nome ou CNPJ" oninput="filtrarPorNomeOuCNPJ()">
+    </div>
+
+    <div class="select-container" title="Filtrar por município">
+      <span>Município:</span> <br>
+      <label for="municipio"> 
+        <i class="fas fa-city"></i> 
+        <select id="municipio">
+          <option value="todos" selected>Todos</option>
+          <?php
+          require "../includes/dados-conexao.inc.php";
+          require "../includes/conectar.inc.php";
+          $sql = "SELECT DISTINCT municipio FROM $nomeDaTabela1";
+          $result = $conexao->query($sql);
+          $municipios = array(); 
+          if ($result->num_rows > 0) {
+              while($row = $result->fetch_assoc()) {
+                  $municipios[] = $row['municipio']; 
+              }
+          }
+          usort($municipios, function($a, $b) {
+            return strcasecmp($a, $b);
+        });
+          foreach ($municipios as $municipio) {
+              echo "<option value='$municipio'>$municipio</option>";
+          }
+          ?>
+        </select>
+      </label>
+    </div>
+      
+    <div class="select-container" title="Filtrar por convênio">
+      <span>Convênio:</span> <br>
+      <label for="convenio">
+        <i class="fas fa-handshake"></i>  
+        <select id="convenio">
+          <option value="todos" selected>Todos</option>
+          <option value="privada">Privada</option>
+          <option value="filantropica">Filantrópica</option>
+          <option value="convenio_publico_estadual">Convênio Estadual</option>
+          <option value="convenio_publico_municipal">Convênio Municipal</option>
+          <option value="sem_convenio">Sem Convênio</option>
+        </select>
+      </label>
+    </div>
+
+      <!-- Botão para Ordenar por Vagas Disponíveis -->
+    <button id="ordenarVagas">Ordenar por vagas</button>
+  </section>
+
   <section class="section-dados-gerais">
     <h1 class='h1-estilizado'> Dados Gerais </h1>
-
-    <!-- Botão para Ordenar por Vagas Disponíveis -->
-    <button id="ordenarVagas">Ordenar Vagas</button>
   
     <?php
     require "../includes/dados-conexao.inc.php";
@@ -35,8 +86,8 @@
             <th>CNPJ</th>
             <th>Nome</th>
             <th>Município</th>
-            <th>Capacidade de Acolhimento</th>
-            <th>Vagas Disponíveis</th>    
+            <th style='text-align: center'>Capacidade de Acolhimento</th>
+            <th style='text-align: center'>Vagas Disponíveis</th>    
             <th>Convênios</th>
           </tr>
         </thead>
@@ -44,11 +95,11 @@
         while($row = $result->fetch_assoc()) {
           $cnpj_ilpi = $row['cnpj'];
           echo "<tr>";
-          echo "<td> <a class='link-tabela-dados-gerais' href='../ilpi/perfilIlpi.php?cnpj_ilpi=$cnpj_ilpi'> {$row['cnpj']} </a> </td>";
-          echo "<td> <a class='link-tabela-dados-gerais' href='../ilpi/perfilIlpi.php?cnpj_ilpi=$cnpj_ilpi'> {$row['nome']} </a> </td>";
+          echo "<td> <div style='width: 120px' class='link-tabela-dados-gerais'> <a href='../ilpi/perfilIlpi.php?cnpj_ilpi=$cnpj_ilpi'> {$row['cnpj']} </a> </div> </td>";
+          echo "<td> <div style='width: 180px' class='link-tabela-dados-gerais'> <a href='../ilpi/perfilIlpi.php?cnpj_ilpi=$cnpj_ilpi'> {$row['nome']} </a> </div> </td>";
           echo "<td>".$row['municipio']."</td>";
-          echo "<td>".$row['capacidade_acolhimento']."</td>";
-          echo "<td>".$row['vagas']."</td>";
+          echo "<td style='text-align: center'>".$row['capacidade_acolhimento']."</td>";
+          echo "<td style='text-align: center'>".$row['vagas']."</td>";
           echo "<td>";
           echo $row['privada'] == 1 ? '-Privada<br>' : '';
           echo $row['filantropica'] == 1 ? '-Filantrópica<br>' : '';
@@ -67,22 +118,99 @@
     }
     ?>
     <script>
-    document.getElementById('ordenarVagas').addEventListener('click', function() {
-      var linhas = Array.from(document.querySelectorAll('.tabela-dados-gerais tbody tr'));
-      
-      linhas.sort(function(a, b) {
-        var vagasA = parseInt(a.querySelector('td:nth-child(5)').textContent);
-        var vagasB = parseInt(b.querySelector('td:nth-child(5)').textContent);
-        return vagasB - vagasA;
+  document.getElementById('ordenarVagas').addEventListener('click', function() {
+    var linhas = Array.from(document.querySelectorAll('.tabela-dados-gerais tbody tr'));
+    
+    linhas.sort(function(a, b) {
+      var vagasA = parseInt(a.querySelector('td:nth-child(5)').textContent);
+      var vagasB = parseInt(b.querySelector('td:nth-child(5)').textContent);
+      return vagasB - vagasA;
+    });
+
+    var tbody = document.querySelector('.tabela-dados-gerais tbody');
+    tbody.innerHTML = '';
+    linhas.forEach(function(linha) {
+      tbody.appendChild(linha);
+    });
+  });
+
+  function filtrarConvenio() {
+    var filtrosSelecionados = Array.from(document.getElementById('convenio').selectedOptions).map(option => option.value);
+    var filtroMunicipio = removerAcentos(document.getElementById('municipio').value.toLowerCase());
+
+    var linhas = Array.from(document.querySelectorAll('.tabela-dados-gerais tbody tr'));
+
+    linhas.forEach(function(linha) {
+      var convenios = linha.querySelector('td:nth-child(6)').innerHTML;
+
+      var mostrarPorConvenio = filtrosSelecionados.includes('todos') || filtrosSelecionados.some(function(filtro) {
+        if (filtro === 'sem_convenio') {
+          return convenios === 'Não há convênios';
+        } else if (filtro === 'privada') {
+          return convenios.includes('-Privada');
+        } else if (filtro === 'filantropica') {
+          return convenios.includes('-Filantrópica');
+        } else if (filtro === 'convenio_publico_estadual') {
+          return convenios.includes('-Convênio Estadual');
+        } else if (filtro === 'convenio_publico_municipal') {
+          return convenios.includes('-Convênio Municipal');
+        }
       });
 
-      var tbody = document.querySelector('.tabela-dados-gerais tbody');
-      tbody.innerHTML = '';
-      linhas.forEach(function(linha) {
-        tbody.appendChild(linha);
-      });
+      var municipio = removerAcentos(linha.querySelector('td:nth-child(3)').textContent.trim().toLowerCase());
+      var mostrarPorMunicipio = (filtroMunicipio === 'todos' || municipio === filtroMunicipio);
+
+      linha.style.display = mostrarPorConvenio && mostrarPorMunicipio ? '' : 'none';
     });
-  </script>
+  }
+
+  //Município
+  function removerAcentos(str) {
+    return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+  }
+
+  document.getElementById('municipio').addEventListener('change', function() {
+    var filtroSelecionado = removerAcentos(this.value.toLowerCase()); // Converter para minúsculas e remover acentos
+    var linhas = Array.from(document.querySelectorAll('.tabela-dados-gerais tbody tr'));
+
+    linhas.forEach(function(linha) {
+      var municipio = removerAcentos(linha.querySelector('td:nth-child(3)').textContent.trim().toLowerCase()); // Converter para minúsculas e remover acentos
+      var mostrarLinha = (filtroSelecionado === 'todos' || municipio === filtroSelecionado);
+      linha.style.display = mostrarLinha ? '' : 'none';
+    });
+
+    // Resetar o filtro de convênio para "Todos"
+    document.getElementById('convenio').value = 'todos';
+
+    // Filtrar novamente após a mudança no município
+    filtrarConvenio();
+  });
+
+  document.getElementById('convenio').addEventListener('change', function() {
+    filtrarConvenio();
+  });
+
+  function filtrarPorNomeOuCNPJ() {
+    // Definir filtros de convênio e município como "todos"
+    document.getElementById('convenio').value = 'todos';
+    document.getElementById('municipio').value = 'todos';
+
+    var filtro = document.getElementById('filtro').value.trim().toLowerCase();
+    var linhas = Array.from(document.querySelectorAll('.tabela-dados-gerais tbody tr'));
+
+    linhas.forEach(function(linha) {
+      var nome = linha.querySelector('td:nth-child(2)').textContent.trim().toLowerCase(); // Coluna do nome
+      var cnpj = linha.querySelector('td:nth-child(1)').textContent.trim().toLowerCase(); // Coluna do CNPJ
+
+      // Verifica se o nome ou o CNPJ contém o termo de busca
+      var mostrarLinha = nome.includes(filtro) || cnpj.includes(filtro);
+
+      // Exibe ou oculta a linha com base no filtro
+      linha.style.display = mostrarLinha ? '' : 'none';
+    });
+  }
+</script>
+
   </section>
 </body> 
 </html> 
